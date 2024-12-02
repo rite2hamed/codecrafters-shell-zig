07b2aa2efe2f0dce221ef4bf8d63bb11c2b23f32
Mohammed Hamed <rite2hamed@gmail.com>
2024-12-02 12:19:10 -0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,16 @@ const std = @import("std");
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
+    while (true) {
+        try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
-    const cmd = user_input;
-    std.debug.print("{s}: not found\n", .{cmd});
+        var it = std.mem.split(u8, user_input, " ");
+        const cmd = it.next().?;
+        // TODO: Handle user input
+        std.debug.print("{s}: not found\n", .{cmd});
+    }
 }
